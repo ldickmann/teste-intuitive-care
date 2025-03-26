d559e6c3821ec224b04c641b21a2d8c370848a3b
ldickmann <ldickmann12@gmail.com>
2025-03-26 01:21:32 -0300

    FIX: Definição de diretório de PDFs definido em web_scraping.py

--- web_scraping/scripts/web_scraping.py
+++ web_scraping/scripts/web_scraping.py
@@ -1,7 +1,12 @@
+import os
 import logging
-from scripts.utils import setup_logging, setup_driver, extract_pdf_links
-from scripts.download_pdfs import download_pdfs
-from scripts.zip_pdfs import zip_pdfs
+from .utils import setup_logging, setup_driver, extract_pdf_links
+from .download_pdfs import download_pdfs
+from .zip_pdfs import zip_pdfs
+
+# Define o diretório de PDFs relativo ao local deste script
+CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
+PDF_DIR = os.path.join(CURRENT_DIR, "..", "pdfs")
 
 # Configuração do logging
 setup_logging()
@@ -23,9 +28,9 @@ finally:
     # Fechamento do WebDriver
     driver.quit()
 
-# Download dos PDFs
-pdf_files = download_pdfs(pdf_links, "pdfs")
+# Download dos PDFs usando o diretório absoluto definido
+pdf_files = download_pdfs(pdf_links, PDF_DIR)
 
-# Compactação dos PDFs em um arquivo ZIP
-zip_filename = "anexos_pdfs.zip"
+# Compactação dos PDFs em um arquivo ZIP dentro da pasta pdfs
+zip_filename = os.path.join(PDF_DIR, "anexos_pdfs.zip")
 zip_pdfs(pdf_files, zip_filename)
