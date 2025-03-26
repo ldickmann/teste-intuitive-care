9d7d09d851fc5bb21b9d97ec53916f0aa5c5b7bd
ldickmann <ldickmann12@gmail.com>
2025-03-26 13:26:24 -0300

    FIX: Desfiz a exclusão equivocada do arquivo main.py & ajuste em transform_data.py para separar os dados em colunas no .csv

--- data_transformation/scripts/transform_data.py
+++ data_transformation/scripts/transform_data.py
@@ -49,7 +49,7 @@ def transform_and_save(data, output_path, nome):
         df["AMB"] = df["AMB"].replace({"AMB": "Seg. Ambulatorial"})
 
         # Salva o DataFrame em um arquivo CSV e codificação UTF-8
-        df.to_csv(output_path, index=False, encoding="utf-8")
+        df.to_csv(output_path, index=False, sep=";", encoding="utf-8")
 
         logging.info(f"Dados transformados e arquivo CSV salvo: {output_path}")
         return True
