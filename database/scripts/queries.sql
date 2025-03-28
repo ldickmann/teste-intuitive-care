388fa8a895c7c35a5253a0070eaae05f70d8632f
ldickmann <ldickmann12@gmail.com>
2025-03-28 16:12:57 -0300

    FEAT: Query para consulta das 10 operadoras com maiores despesas

---
+++ database/scripts/queries.sql
@@ -0,0 +1,26 @@
+-- 10 operadoras com maiores despesas no último ano - Query para consulta
+SELECT
+  d.registro_ans,
+  o.Razao_Social,
+  SUM(d.saldo_final - d.saldo_inicial) AS total_despesas
+FROM
+  despesas_operadoras d
+  JOIN operadoras_ans o ON d.registro_ans = o.Registro_ANS
+WHERE
+  d.descricao LIKE '%SINISTROS CONHECIDOS%'
+  AND d.data_movimentacao >= DATE_SUB(
+    (
+      SELECT
+        MAX(data_movimentacao)
+      FROM
+        despesas_operadoras
+    ),
+    INTERVAL 12 MONTH
+  )
+GROUP BY
+  d.registro_ans,
+  o.Razao_Social
+ORDER BY
+  total_despesas DESC
+LIMIT
+  10;
