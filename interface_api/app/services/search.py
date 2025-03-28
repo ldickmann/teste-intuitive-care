e0e864a9a22ff16ed3536c7e0ab342fecec5ea2e
ldickmann <ldickmann12@gmail.com>
2025-03-28 20:17:33 -0300

    FEAT: Script para buscas codificado, classes adicionadas

---
+++ interface_api/app/services/search.py
@@ -0,0 +1,71 @@
+# Logica para buscar as operadoras
+
+import pandas as pd
+import app.core.config as settings
+import app.models.operadora as Operadora
+from typing import List
+import unidecode
+
+
+class OperadoraService:
+    def __init__(self):
+        self.df = self._carregar_dados()
+
+    def _carregar_dados(self) -> pd.DataFrame:
+        """Carrega o CSV e ajusta a formatação."""
+        try:
+            df = pd.read_csv(
+                settings.DATA_PATH, delimiter="\t", encoding="latin1", dtype=str
+            )
+            df.columns = [
+                "registro_ans",
+                "cnpj",
+                "razao_social",
+                "nome_fantasia",
+                "modalidade",
+                "logradouro",
+                "numero",
+                "complemento",
+                "bairro",
+                "cidade",
+                "uf",
+                "cep",
+                "ddd",
+                "telefone",
+                "fax",
+                "email",
+                "representante",
+                "cargo_representante",
+                "regiao_comercializacao",
+                "data_registro_ans",
+            ]
+
+            # Limpeza e ajustes
+            df = df.fillna("")  # Preencher NaN com string vazia
+            df["nome_fantasia"] = df["nome_fantasia"].apply(
+                unidecode.unidecode
+            )  # Remover acentos
+            df["cidade"] = df["cidade"].apply(unidecode.unidecode)  # Normalizar cidades
+            df["uf"] = df["uf"].str.upper()  # Garantir maiúsculas
+            return df
+        except Exception as e:
+            print(f"Erro ao carregar CSV: {e}")
+            return pd.DataFrame()
+
+    def buscar_operadoras(self, termo: str) -> List[Operadora]:
+        """Busca operadoras que contenham o termo no nome, cidade ou CNPJ."""
+        termo_normalizado = unidecode.unidecode(termo).lower()
+        resultados = self.df[
+            self.df["nome_fantasia"]
+            .str.lower()
+            .str.contains(termo_normalizado, na=False)
+            | self.df["razao_social"]
+            .str.lower()
+            .str.contains(termo_normalizado, na=False)
+            | self.df["cnpj"].str.contains(termo_normalizado, na=False)
+            | self.df["cidade"].str.lower().str.contains(termo_normalizado, na=False)
+        ]
+        return [Operadora(**row.to_dict()) for _, row in resultados.iterrows()]
+
+
+operadora_service = OperadoraService()
