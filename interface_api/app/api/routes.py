5a0b5a3076fc33b40a5c7a5f352c28448da19aba
ldickmann <ldickmann12@gmail.com>
2025-03-28 23:00:35 -0300

    FEAT: Código das rotas adicionado

--- interface_api/app/api/routes.py
+++ interface_api/app/api/routes.py
@@ -1,7 +1,22 @@
 # Definição das rotas da API
 
 from fastapi import APIRouter, Query
+from app.services.search import operadora_service
 from typing import List
 from app.models.operadora import Operadora
 
 router = APIRouter()
+
+
+@router.get("/buscar", response_model=List[Operadora])
+def buscar_operadoras(
+    nome: str = Query(None, min_length=3, description="Nome da operadora"),
+    cidade: str = Query(None, description="Cidade da operadora"),
+    cnpj: str = Query(None, description="CNPJ da operadora"),
+):
+    """Busca operadoras por nome, cidade ou CNPJ."""
+    if not any([nome, cidade, cnpj]):
+        return []  # Retorna lista vazia se nenhum parâmetro for fornecido
+
+    termo = nome or cidade or cnpj
+    return operadora_service.buscar_operadoras(termo)
